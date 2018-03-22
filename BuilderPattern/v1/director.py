66506ade03a239cabc93d817bf0f95bca633d73e
Dave Gines <dgines@rentdynamics.com>
2018-03-21 21:58:37 -0600

    Builder clean up

--- BuilderPattern/v1/director.py
+++ BuilderPattern/v1/director.py
@@ -1,9 +1,5 @@
 class Director:
-    def __init__(self):
-        self.builder = None
-
     def construct(self, builder):
-        self.builder = builder
-        self.builder.build_header()
-        self.builder.build_body()
-        self.builder.build_footer()
+        builder.build_header()
+        builder.build_body()
+        builder.build_footer()
