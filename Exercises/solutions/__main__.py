5a6fe0eb720abe851adecf23dc59e9481b3a48c1
Dave Gines <dgines@rentdynamics.com>
2018-10-02 23:15:35 -0600

    Sample problems

--- Exercises/solutions/__main__.py
+++ Exercises/solutions/__main__.py
@@ -1,3 +1,29 @@
+import collections
+
+
+'''rotate 2 dimensional array 90 degrees clockwise'''
+def rotate(two_dimensional_array):
+    array_items_count = len(two_dimensional_array)
+    new_array = []
+    x = 0
+    while x < array_items_count:
+        sub_array = ''
+        y = array_items_count - 1
+        while y >= 0:
+            sub_array += str(two_dimensional_array[y][x])
+            if y > 0:
+                sub_array += ', '
+            y -= 1
+        new_array.append([sub_array])
+        x += 1
+    return new_array
+
+
+print(rotate([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]))
+
+
 '''Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
 
 Example
@@ -15,13 +41,7 @@ def adjacent_elements_product(input_array):
         index += 1
     return largest_product if largest_product != -99999 else None
 
-print(adjacent_elements_product([3, 6, -2, -5, 7, 3]))  # expect result = 21
-print(adjacent_elements_product([1, -2, 2]))  # expect result = -2
-print(adjacent_elements_product([1]))  # expect result = None
 
-
-def is_palindrome(word):
-    return word == word[::-1]
-
-print(is_palindrome('dad'))  # expect True
-print(is_palindrome('Max'))  # expect False
+# print(adjacent_elements_product([3, 6, -2, -5, 7, 3]))  # expect result = 21
+# print(adjacent_elements_product([1, -2, 2]))  # expect result = -2
+# print(adjacent_elements_product([1]))  # expect result = None
