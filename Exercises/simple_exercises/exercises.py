a87c908d5fd19b5fefcdc6ddeba9ef92a236fd30
Dave Gines <dgines@rentdynamics.com>
2019-08-22 14:26:54 -0600

    Code kata problems

--- Exercises/simple_exercises/exercises.py
+++ Exercises/simple_exercises/exercises.py
@@ -1,4 +1,157 @@
+from datetime import datetime, timedelta
+import re
+
+
 class SampleExercises:
+    # Dept meeting Aug 22, 2019
+    @staticmethod
+    def get_roman_numeral(int_number):
+        return ''
+
+    # Dept meeting Aug 22, 2019
+    @staticmethod
+    def remove_duplicate_words(phrase):
+        return ''
+
+    # Dept meeting Aug 22, 2019
+    @staticmethod
+    def dict_replacer(phrase, dictionary):
+        return ''
+
+
+
+
+    @staticmethod
+    def dynamic_classes(names):
+        new_classes = []
+
+        @classmethod
+        def is_the_one(cls):
+            return cls.__name__.lower() == 'neo'
+
+        for n in names:
+            new_classes.append(type(n, (object,), {'is_the_one': is_the_one}))
+
+        return new_classes
+
+        """get_dynamic_classes: Needs to return a list of classes that are dynamically created that each have a 
+        function named is_the_one that will return true if the class name is Neo otherwise false.
+        """
+
+    @staticmethod
+    def likes_and_dislikes(text):
+        developer_likes_and_dislikes = """+------------------------------------+-----------------------------------+
+| likes                              | dislikes                          |
++------------------------------------+-----------------------------------+
+| Meritocracy                        | Favoritism, ass-kissing, politics |
++------------------------------------+-----------------------------------+
+| Healthy debates and collaboration  | Ego-driven rhetoric, drama and FUD|
+|                                    | to get one's way                  |
++------------------------------------+-----------------------------------+
+| Autonomy given by confident leaders| Micro-management by insecure      |
+| capable of attracting top-tier     | managers compensating for a weak, |
+| talent                             | immature team                     |
++------------------------------------+-----------------------------------+
+| Fluid communication, brief, ad-hoc | Formal meetings, endless debate   |
+| discussions, white-boarding, and   |                                   |
+| quick but informed decisions       |                                   |
++------------------------------------+-----------------------------------+
+| Clever and disruptive business     |                                   |
+| ideas that solve real and immediate|                                   |
+| needs in a marketplace             |                                   |
++------------------------------------+-----------------------------------+
+"""
+        likes_and_dislikes = []
+        records = developer_likes_and_dislikes.split('+------------------------------------+-----------------------------------+')
+
+        for record in records[2:]:
+            lines = record.split('\n')
+            like = ''
+            dislike = ''
+
+            for line in lines[1:-1]:
+                like += line[2: 37].rstrip() + ' '
+                dislike += line[39: -1].rstrip() + ' '
+            likes_and_dislikes.append((like.rstrip(), dislike.rstrip()))
+
+    @staticmethod
+    def month_last_day(dt):
+        if dt.month == 12:
+            return 31
+        last_day = dt.replace(month=dt.month + 1, day=1) - timedelta(days=1)
+        return last_day.day
+        """
+        month_last_day: Needs to return the last day of the month as an integer
+        :return:
+        """
+
+    @staticmethod
+    def week_start_end(dt):
+        today = dt
+        start = today - timedelta(days=today.weekday())
+        end = start + timedelta(days=6)
+        return (datetime.combine(start, datetime.min.time()), datetime.combine(end, datetime.max.time()))
+
+    @staticmethod
+    def dict_key_values_swapped(d):
+        resp = {}
+        for i in dict.items(d):
+            resp[i[1]] = i[0]
+        return resp
+        """
+        reverse_dict: Needs to return the dictionary provided by with the values as the keys and the keys as the values
+        """
+
+    @staticmethod
+    def string_parse(text):
+        likes_and_dislikes = []
+        records = text.split('+------------------------------------+-----------------------------------+')
+
+        for record in records[2:]:
+            if record.find('|') < 0:
+                continue
+            start_idx = record.find('|') + 1
+            start_break_idx = record.index('|', start_idx) - 1
+            end_idx = record.index('|', start_break_idx + 2) - 1
+            end_break_idx = start_break_idx + 2
+            lines = record.split('\n')[1: -1]
+            like = ''
+            dislike = ''
+
+            for line in lines:
+                like += line[start_idx: start_break_idx].rstrip() + ' '
+                dislike += line[end_break_idx: end_idx].rstrip() + ' '
+            likes_and_dislikes.append((like.rstrip(), dislike.rstrip()))
+
+        return likes_and_dislikes
+
+    @staticmethod
+    def sorted_dict_desc(fruit_dict):
+        return sorted(fruit_dict, key=lambda f: f['count'])
+
+    @staticmethod
+    def greater_than_avg(nums):
+        avg = sum(nums) / len(nums)
+        greater_than_avg = [n for n in nums if n > avg]
+        return greater_than_avg
+
+        """
+        :param nums:
+        :return:
+        self.assertEqual([3], self.solutions.greater_than_avg([1, 2, 3]))
+        self.assertEqual([8, 5, 11], self.solutions.greater_than_avg([-1, 8, 0, 4, 5, 2, 11]))
+        """
+
+    @staticmethod
+    def is_palindrome():
+        def test_function(word):
+            re_alpha_only = '[^A-Za-z0-9]+'
+            w1 = re.sub(re_alpha_only, '', word.lower())
+            w2 = re.sub(re_alpha_only, '', word[::-1].lower())
+
+            return w1 == w2
+        return test_function
+
     @staticmethod
     def word_wrap(word, wrap_at):
         length_of_word = len(word)
--- Exercises/simple_exercises/test_exercises.py
+++ Exercises/simple_exercises/test_exercises.py
@@ -1,8 +1,162 @@
 import unittest
+from datetime import datetime
 from Exercises.simple_exercises.exercises import SampleExercises as se
 
 
 class SimpleTest(unittest.TestCase):
+    # Remove duplicate words
+    def test_remove_duplicate_words_none_exist(self):
+        phrase = 'alpha'
+        result = se.remove_duplicate_words(phrase)
+        self.assertEqual('alpha', result)
+
+    def test_remove_duplicate_words_one_duplicate(self):
+        phrase = 'alpha beta alpha'
+        result = se.remove_duplicate_words(phrase)
+        self.assertEqual('alpha beta', result)
+
+    def test_remove_duplicate_words_multiple_duplicates(self):
+        phrase = 'alpha beta alpha alpha beta delta alpha'
+        result = se.remove_duplicate_words(phrase)
+        self.assertEqual('alpha beta delta', result)
+
+
+    # Dictionary replacer
+    def test_dict_replacer_no_phrase(self):
+        phrase = ''
+        dictionary = {}
+        result = se.dict_replacer(phrase, dictionary)
+        self.assertEqual('', result)
+
+    def test_dict_replacer_one_item_phrase(self):
+        phrase = '<a>'
+        dictionary = {
+            'a': 'successful'
+        }
+        result = se.dict_replacer(phrase, dictionary)
+        self.assertEqual('successful', result)
+
+    def test_dict_replacer_two_items_phrase(self):
+        phrase = 'I think <1> lives in <2>.'
+        dictionary = {
+            '1': 'Nate',
+            '2': 'Idaho'
+        }
+        result = se.dict_replacer(phrase, dictionary)
+        self.assertEqual('I think Nate lives in Idaho.', result)
+
+
+    # Roman numeral
+    def test_get_roman_numeral_one(self):
+        roman_numeral = se.get_roman_numeral(1)
+        self.assertEqual('I', roman_numeral)
+
+    def test_get_roman_numeral_three(self):
+        roman_numeral = se.get_roman_numeral(3)
+        self.assertEqual('III', roman_numeral)
+
+    def test_get_roman_numeral_four(self):
+        roman_numeral = se.get_roman_numeral(4)
+        self.assertEqual('IV', roman_numeral)
+
+    def test_get_roman_numeral_five(self):
+        roman_numeral = se.get_roman_numeral(5)
+        self.assertEqual('V', roman_numeral)
+
+    def test_get_roman_numeral_six(self):
+        roman_numeral = se.get_roman_numeral(6)
+        self.assertEqual('VI', roman_numeral)
+
+    def test_get_roman_numeral_nine(self):
+        roman_numeral = se.get_roman_numeral(9)
+        self.assertEqual('IX', roman_numeral)
+
+    def test_get_roman_numeral_ten(self):
+        roman_numeral = se.get_roman_numeral(10)
+        self.assertEqual('X', roman_numeral)
+
+
+
+
+    def test_palindrome(self):
+        test_function = se.is_palindrome()
+        self.assertTrue(test_function('Doc, note: I dissent. A fast never prevents a fatness. I diet on cod'))
+        self.assertFalse(test_function('I am not one for sure'))
+        self.assertTrue(test_function('Able was I ere I saw Elba'))
+        self.assertTrue(test_function('Never odd or even'))
+        self.assertTrue(test_function('aabbccddeefeeddccbbaa'))
+        self.assertFalse(test_function('abbccddeeffeddccbbaa'))
+
+    def test_string_parse(self):
+        developer_likes_and_dislikes = """
+
+        +------------------------------------+-----------------------------------+
+        | likes                              | dislikes                          |
+        +------------------------------------+-----------------------------------+
+        | Meritocracy                        | Favoritism, ass-kissing, politics |
+        +------------------------------------+-----------------------------------+
+        | Healthy debates and collaboration  | Ego-driven rhetoric, drama and FUD|
+        |                                    | to get one's way                  |
+        +------------------------------------+-----------------------------------+
+        | Autonomy given by confident leaders| Micro-management by insecure      |
+        | capable of attracting top-tier     | managers compensating for a weak, |
+        | talent                             | immature team                     |
+        +------------------------------------+-----------------------------------+
+        | Fluid communication, brief, ad-hoc | Formal meetings, endless debate   |
+        | discussions, white-boarding, and   |                                   |
+        | quick but informed decisions       |                                   |
+        +------------------------------------+-----------------------------------+
+        | Where else can I help out?         | I'm blocked by..., I only know how|
+        |                                    | to...                             |
+        +------------------------------------+-----------------------------------+
+        | Getting things done.               | Contrived company culture         |
+        +------------------------------------+-----------------------------------+
+        | Clever and disruptive business     |                                   |
+        | ideas that solve real and immediate|                                   |
+        | needs in a marketplace             |                                   |
+        +------------------------------------+-----------------------------------+
+        | Software and system abstractions   | Hard-coding, brute-force          |
+        +------------------------------------+-----------------------------------+
+        | Frameworks and best-practices      | Hermetic code-base                |
+        +------------------------------------+-----------------------------------+
+        | Best tool for the job              | One size fits all                 |
+        +------------------------------------+-----------------------------------+
+        | Simple design                      | Over-engineering                  |
+        +------------------------------------+-----------------------------------+
+        | Leveraging open-source             | Re-inventing the wheel            |
+        +------------------------------------+-----------------------------------+
+        | Practical solutions to business    | Let's do this or use that because |
+        | core competency                    | it's new and cool                 |
+        +------------------------------------+-----------------------------------+
+        | Building solutions to current      | Over-emphasizing "nice-to-haves"  |
+        | business needs and customer demand | and conjecture                    |
+        +------------------------------------+-----------------------------------+
+
+        """
+        answer = se.string_parse(developer_likes_and_dislikes)
+        self.assertEqual(answer, [
+            (u'Meritocracy', u'Favoritism, ass-kissing, politics'),
+            (u'Healthy debates and collaboration',
+             u"Ego-driven rhetoric, drama and FUD to get one's way"),
+            (u'Autonomy given by confident leaders capable of attracting top-tier talent',
+             u'Micro-management by insecure managers compensating for a weak, immature team'),
+            (u'Fluid communication, brief, ad-hoc discussions, white-boarding, and quick but informed decisions',
+             u'Formal meetings, endless debate'),
+            (u'Where else can I help out?', u"I'm blocked by..., I only know how to..."),
+            (u'Getting things done.', u'Contrived company culture'),
+            (u'Clever and disruptive business ideas that solve real and immediate needs in a marketplace',
+             u''),
+            (u'Software and system abstractions', u'Hard-coding, brute-force'),
+            (u'Frameworks and best-practices', u'Hermetic code-base'),
+            (u'Best tool for the job', u'One size fits all'),
+            (u'Simple design', u'Over-engineering'),
+            (u'Leveraging open-source', u'Re-inventing the wheel'),
+            (u'Practical solutions to business core competency',
+             u"Let's do this or use that because it's new and cool"),
+            (u'Building solutions to current business needs and customer demand',
+             u'Over-emphasizing "nice-to-haves" and conjecture')
+        ])
+
     # word wrap
     def test_word_wrap_should_return_the_word_if_column_wrap_number_is_greater_than_length_of_word(self):
         wrapped_content = se.word_wrap(word='a', wrap_at=100)
